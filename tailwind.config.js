2a2a3eb291ba6c9c8cbbc535e44347d85f5a59a4
rajaboff007 <asadbekrajaboff1337@gmail.com>
2024-11-01 18:04:28 +0500

    --v1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'main-blue': '#315fe9',
+        'main-silver' : '#869ab8'
+      }
+    },
+  },
+  plugins: [],
+}
+
